1de60b14ac54629ae586655f09964b321fbb1182
atwoodamanda <atwooda86@gmail.com>
2014-10-24 13:09:43 -0700

    Mailbox Homework

---
+++ Mailbox/FeedViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FeedViewController.swift
+//  Mailbox
+//
+//  Created by Amanda Atwood on 10/24/14.
+//  Copyright (c) 2014 Amanda Atwood. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 1346)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
